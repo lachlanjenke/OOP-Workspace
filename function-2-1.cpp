b680f4dd0240fc466dd529ed50495aa0da0872b3
a1924627 <a1924627@adelaide.edu.au>
2024-07-25 22:53:22 +0930

    Added if n is less than 0

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -4,6 +4,10 @@ using namespace std; // Specifying namespace
 int min_element(int array[], int n) { 
     int min = array[0]; // Initialise variable
 
+    if (n < 1) { // If n is less than one
+        return 0; 
+    }
+
     for (int i = 0; i < n; i++) { // Finding min element 
         if (array[i] < min) {
             min = array[i]; 
