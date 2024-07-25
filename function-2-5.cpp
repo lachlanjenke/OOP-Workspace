425f17364e1e5dda971017dca9e3d676436b741d
a1924627 <a1924627@adelaide.edu.au>
2024-07-25 23:46:01 +0930

    q2.5

---
+++ function-2-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // Include libraries
+using namespace std; // Specifying namespace#include <iostream> // Include libraries
+using namespace std; // Specifying namespace
+
+bool is_descending(int array[], int n) {
+
+    if (n < 1) { // if n is less than 1
+        return false; 
+    }
+
+    for (int i = 1; i < n; i++) { // Check if descending
+        if (array[i] > array[i-1]) {
+            return false; 
+        }
+
+    }
+
+    return true; // Return true if descending
+}
---
+++ main-2-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // Include libraries
+using namespace std; // Specifying namespace
+
+extern bool is_descending(int array[], int n); // Specify where function is
+
+int main() {
+    int array[7] = {7,6,5,4,3,2,1};
+    int n = sizeof(array)/sizeof(int); 
+
+    cout << "1 if the array is descending, 0 if not: " << is_descending(array,n) << endl; 
+
+    return 0; // End code 
+
+
+}
