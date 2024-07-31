fbc605b655d7b7e89a948655ff5632750b3b5a0f
a1924627 <a1924627@adelaide.edu.au>
2024-07-31 23:40:05 +0930

    1st commit for 1.2 with manual size input

---
+++ main-1-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream> // Include libraries
+using namespace std; // Specifying namespace
+
+extern int is_identity(int array[10][10]); 
+
+int main() {
+    int matrix[10][10] = {
+        {1,0,0,0,0,0,0,0,0,0},
+        {0,1,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0,0},
+        {0,0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,0,1,0,0,0,0},
+        {0,0,0,0,0,0,1,0,0,0},
+        {0,0,0,0,0,0,0,1,0,0},
+        {0,0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0,1},
+    };
+
+    cout << "Return 1 if the matrix is an indentity matrix, 0 if not: " << is_identity(matrix) << endl; 
+
+    return 0; 
+}
