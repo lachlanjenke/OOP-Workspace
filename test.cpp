ae8e2da2143e97990bc95c5dcad759795a6449ad
a1924627 <a1924627@adelaide.edu.au>
2024-08-01 12:37:19 +0930

    removed

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-  int arr[] = {10,20,30,40,50,60};
-  int arrSize = sizeof(arr)/sizeof(arr[0]);
-  cout << "The size of the array is: " << arrSize;
-  return 0;
-}
