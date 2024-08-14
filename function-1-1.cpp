3e6821121277db22d9759583a937b2e170167b5d
a1924627 <a1924627@adelaide.edu.au>
2024-08-14 22:28:01 +0930

    Removed length variable from readNumbers

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,18 +2,13 @@
 using namespace std; // Namespace
 
 int *readNumbers() {
-    int length = 10; // Initialise variables
 
-    if (length < 1) { // If length 0 or negative
-        return nullptr; 
-    }
-
-    int *numbers_arr = new int[length]; // Dynamically initialise array
+    int *numbers_arr = new int[10]; // Dynamically initialise array
 
     cout << "Input the first value: "; // Prompt user for the first value
     cin >> numbers_arr[0]; 
 
-    for (int i = 1; i < length; i++) { // Prompt user for the other values
+    for (int i = 1; i < 10; i++) { // Prompt user for the other values
         cout << "Input the next value: "; 
         cin >> numbers_arr[i]; 
     }
@@ -22,11 +17,9 @@ int *readNumbers() {
 }
 
 void printNumbers(int *numbers,int length) {
-
-    if (length >= 1) { // If length 0 or negative
+  
         for (int i = 0; i < length; i++) { // Printing values
             cout << i << " " << numbers[i] << endl; 
         }
-    }
 
 }
