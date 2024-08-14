8aea47769115b8b6a16f304345c4b124700219a3
a1924627 <a1924627@adelaide.edu.au>
2024-08-14 22:44:41 +0930

    Didnt work

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,26 +2,19 @@
 using namespace std; // Namespace
 
 int *readNumbers() {
-    int length = 10; // Initialise variables
 
-    if (length < 1) { // If length 0 or negative
+    int *numbers_arr = new int[10]; // Dynamically initialise array
+
+    if (numbers_arr == nullptr) { // If memory allocation
         return nullptr; 
     }
 
-    int *numbers_arr = new int[length]; // Dynamically initialise array
-
     cout << "Input the first value: "; // Prompt user for the first value
-    if (!(cin >> numbers_arr[0])) {
-        delete[] numbers_arr; // Free memory in case of error
-        return nullptr;
-    }
+    cin >> numbers_arr[0]; 
 
-    for (int i = 1; i < length; i++) { // Prompt user for the other values
+    for (int i = 1; i < 10; i++) { // Prompt user for the other values
         cout << "Input the next value: "; 
-        if (!(cin >> numbers_arr[i])) {
-            delete[] numbers_arr; // Free memory in case of error
-            return nullptr;
-        }
+        cin >> numbers_arr[i]; 
     }
 
     return numbers_arr; // Return pointer
