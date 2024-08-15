c2d30c1feb80fc5191f8d9ca09a39547f7d60af2
a1924627 <a1924627@adelaide.edu.au>
2024-08-15 21:19:00 +0930

    Q1-2

--- Person.h
+++ Person.h
@@ -1,9 +1,17 @@
+#include <string>  // Library
+using namespace std; 
+
 #ifndef Person_H // Header guard
 #define Person_H
 
 struct Person {
     string name; 
     int age;
+};
+
+struct PersonList {
+    Person* people;
+    int numPeople;
 };
 
 #endif
