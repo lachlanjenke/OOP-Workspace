5a571abcb5f8aff21243a9548ec4568ac2665d4b
a1924627 <a1924627@adelaide.edu.au>
2024-08-20 11:55:38 +0930

    1st attempt at 1st question

---
+++ Musician.cpp
@@ -0,0 +1,21 @@
+#include "Musician.h"
+
+using namespace std;
+
+Musician::Musician() {
+    instrument = "Null"; 
+    experience = 0; 
+}
+
+Musician::Musician(string input_instrument, int input_experience) {
+    instrument = input_instrument; 
+    experience = input_experience; 
+}
+
+string Musician::get_instrument() {
+    return instrument; 
+}
+
+int Musician::get_experience() {
+    return experience;     
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Musician.h"
+
+using namespace std; 
+
+int main() {
+    Musician m1;
+    cout << "Default instrument: " << m1.get_instrument() << ", Default experience: " << m1.get_experience() << endl;
+
+    Musician m2("guitar", 5);
+    cout << "Instrument: " << m2.get_instrument() << ", Experience: " << m2.get_experience() << std::endl;
+
+    return 0;
+}
