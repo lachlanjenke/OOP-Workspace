09b49b8f2532b6ff50f8ad27f6e4932271538836
a1924627 <a1924627@adelaide.edu.au>
2024-08-22 13:53:38 +0930

    First attempt

---
+++ Store.cpp
@@ -0,0 +1,48 @@
+#include <string.h>
+#include <iostream>
+#include "StockItem.h"
+#include "Store.h"
+
+using namespace std;
+
+// default constructor
+Store::Store() {
+    capacity = 0; 
+    item_list = ""; 
+} 
+
+// constructor for a store that can have up to capacity number of items
+Store::Store(int capacity) { 
+   Store 
+
+}
+
+// returns the number items currently stocked
+// note that if more than one of the same item is in stock they are *all* counted
+// i.e. if 3 widgets and 2 hammers are in stock then it will return 5.
+int Store::get_Total_Stock_Count() {
+    return current_capacity;
+}
+
+// returns the total count of items in stock with the given item code
+// i.e. if there are 3 widgets (with the given item_code in stock then it will return 3.
+int Store::get_Stock_Count(int item_code) {
+
+}
+
+// returns the array of all items in stock. 
+// i.e. if there are 3 widgets and 2 hammers then the returned array will have 5 items.
+StockItem *get_Stock_List() {
+
+}
+
+// returns true and adds new_stock to the store if the store has enough capacity
+// otherwise returns false
+bool add_Stock(StockItem new_stock) {
+    
+
+}
+
+Store::~Store() {
+    
+}
---
+++ main-1-2.cpp
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <iostream>
+#include "StockItem.h"
+#include "Store.h"
+
+using namespace std;
+
+int main() {
+    Store Woolies(5);
+
+}
