00a3819e06c34a80dd11b858dc64f90b75e52fcd
a1924627 <a1924627@adelaide.edu.au>
2024-10-24 14:53:53 +1030

    Game

--- Game.hpp
+++ Game.hpp
@@ -1,8 +1,64 @@
 #ifndef GAME_H
 #define GAME_H
 
-class Game {
+#include <vector>
+#include <iostream>
+#include "Robot.hpp"
+#include "Goal.hpp"
+#include "Obstacle.hpp"
 
+using namespace std;
+
+enum GameState {
+    WIN,
+    LOSE,
+    PLAYING
+};
+
+class Game {
+    private:
+        int width, height;
+        vector<pair<int,int>>obstacleCords;
+        GameState state;
+        Robot *player;
+        Goal *goal;
+        Obstacle *obstacle;
+    public:
+        Game(int width, int height, std::vector<std::pair<int, int>> obstacleCoordinates) {
+            this->width = height;
+            this->height = width;
+            player = new Robot(width,height);
+            goal = new Goal(width,height);
+
+            obstacle = new Obstacle(2,2,width,height);
+
+            state = PLAYING;
+
+        }
+
+        bool displayState() {
+            switch (state) {
+                case WIN:
+                    cout << "You win!" << endl;
+                    break;
+                case LOSE:
+                    cout << "You lose :(" << endl;
+                    break;
+                case PLAYING:
+                    cout << "Game on" << endl;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void movePlayer(int dx, int dy) {
+            player->move(dx,dy);
+        }
+
+        void printGrid() {
+    
+        }
 }; 
 
 #endif
