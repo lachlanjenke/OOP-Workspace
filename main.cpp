c61cfab37032e812a15d246337a7aceb8eea9c28
a1924627 <a1924627@adelaide.edu.au>
2024-11-05 19:33:10 +1030

    m

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+class A {
+    public:
+    A() { cout << "It is in A";}
+};
+
+class B: public A {
+    public:
+    B() { cout << "It is in B";}
+};
+
+int main() {
+    A *a = new B;
+}
